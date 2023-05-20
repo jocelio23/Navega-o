02b7011f8b03e57bdd80de1802b0e20cda77c145
jocelio <joceliotecinfor@gmail.com>
2023-05-20 15:39:38 -0300

    novas paginas

---
+++ resources/views/galeria.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../css/style.css">
+    <title>NaveGate</title>
+</head>
+<body>
+
+<div class="menu-topo">
+    <div class="class-nav">
+        <ul>
+            <li><a href="#">Inserir</a></li>
+            <li><a href="#">Chamados</a></li>
+            <li><a href="#">Pontuações</a></li>
+            <li><a href="#">Notificar</a></li>
+        </ul>
+    </div>
+</div>
+
+    <div class="main-login">
+        
+
+    <div class="responsive">
+  <div class="gallery">
+    <a target="_blank" href="img_5terre.jpg">
+      <img src="../../img/learning-animate.svg" alt="Cinque Terre" width="600" height="400">
+    </a>
+    <div class="desc">Add a description of the image here</div>
+  </div>
+</div>
+
+
+<div class="responsive">
+  <div class="gallery">
+    <a target="_blank" href="img_forest.jpg">
+      <img src="../../img/romantic-getaway-animate.svg" alt="Forest" width="600" height="400">
+    </a>
+    <div class="desc">Add a description of the image here</div>
+  </div>
+</div>
+
+<div class="responsive">
+  <div class="gallery">
+    <a target="_blank" href="img_lights.jpg">
+      <img src="../../img/problem-solving-animate.svg" alt="Northern Lights" width="600" height="400">
+    </a>
+    <div class="desc">Add a description of the image here</div>
+  </div>
+</div>
+
+<div class="responsive">
+  <div class="gallery">
+    <a target="_blank" href="img_mountains.jpg">
+      <img src="../../img/ahmedabad-animate.svg" alt="Mountains" width="600" height="400">
+    </a>
+    <div class="desc">Add a description of the image here</div>
+  </div>
+</div>
+
+<div class="clearfix"></div>
+        
+        <!-- <img src="Astronaut suit-amico.svg" width=400px> -->
+        <!-- <iframe src="https://giphy.com/embed/VOgpnDOAddaN0zr9UE" width="480" height="480" frameBorder="0"  allowFullScreen></iframe> -->
+        <!-- <img src="../../img/astronaut-suit-animate.svg" width="400px"> -->
+
+        <div class="right-login">
+            <div class="card-login">
+                <h1>Eai,Topa?</h1>
+            <div class="text-field">
+                
+                </div>
+                <button class="btn-login">Bora</button>
+            </div>
+        </div>
+    </div>
+
+
+</body>
+</html>
---
+++ resources/views/painel.blade.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../css/style.css">
+    <title>NaveGate</title>
+</head>
+<body>
+
+<div class="menu-topo">
+    <div class="class-nav">
+        <ul>
+            <li><a href="#">Inserir</a></li>
+            <li><a href="#">Chamados</a></li>
+            <li><a href="#">Pontuações</a></li>
+            <li><a href="#">Notificar</a></li>
+        </ul>
+    </div>
+</div>
+    <div class="main-login">
+        <div class="left-text">
+            <div class="chip">
+                <img src="../../img/learning-animate.svg" alt="Person" width="420" height="420">
+                John Doe
+            </div>
+        </div>
+        <!-- <img src="Astronaut suit-amico.svg" width=400px> -->
+        <!-- <iframe src="https://giphy.com/embed/VOgpnDOAddaN0zr9UE" width="480" height="480" frameBorder="0"  allowFullScreen></iframe> -->
+        <!-- <img src="../../img/astronaut-suit-animate.svg" width="400px"> -->
+
+        <div class="right-login">
+            <div class="card-login">
+                <h1>Opa! vamos navegar?</h1>
+            <div class="text-field">
+                <label for="senha">Espaço</label>
+                   <input type="search" name="busca" placeholder="Destino?">
+                </div>
+                <button class="btn-login">Iniciar</button>
+            </div>
+        </div>
+    </div>
+
+
+</body>
+</html>
